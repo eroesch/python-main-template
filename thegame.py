9dc7339aa4025d227ecfc05610aacf0c8723af4f
Etienne Roesch <e.b.roesch@reading.ac.uk>
2020-02-12 15:35:16 +0000

    Added hello world

---
+++ thegame.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 12 15:30:16 2020
+
+@author: tz901918
+"""
+
+def main():
+    """ This is the main function """
+    print("Hello world!")
+    return 0
+
+if __name__ == "__main__":
+    main()
